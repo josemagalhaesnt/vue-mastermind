009da98453b6fc36fccb7e17d5cf181165d38ab9
José Magalhães <josemagalhaesnt@gmail.com>
2021-03-12 18:37:09 -0300

    Added default array for colorSelect

--- app/src/constants/colorConstants/index.js
+++ app/src/constants/colorConstants/index.js
@@ -1,8 +1,26 @@
 const colorConstants = [
-  { colorId: 0, name: "red", hexValue: "#d81159" },
-  { colorId: 1, name: "blue", hexValue: "#2274a5" },
-  { colorId: 2, name: "green", hexValue: "#32936f" },
-  { colorId: 3, name: "yellow", hexValue: "#ffbf00" }
+  { colorId: 0, name: 'grey', hexValue: '#676767' },
+  { colorId: 1, name: 'orange', hexValue: '#ffa500' },
+  { colorId: 2, name: 'red', hexValue: '#d81159' },
+  { colorId: 3, name: 'blue', hexValue: '#2274a5' },
+  { colorId: 4, name: 'green', hexValue: '#32936f' },
+  { colorId: 5, name: 'yellow', hexValue: '#ffbf00' }
 ];
 
-export default colorConstants;
+const defaultSelectedColors = [
+  { id: 0, color: colorConstants[0] },
+  {
+    id: 1,
+    color: colorConstants[0]
+  },
+  {
+    id: 2,
+    color: colorConstants[0]
+  },
+  {
+    id: 3,
+    color: colorConstants[0]
+  }
+];
+
+export { colorConstants, defaultSelectedColors };
